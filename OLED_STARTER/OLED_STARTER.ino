7e1df0071a57f4d997b19bdef669ff05513a4005
Peter Rogers <p.rogers@gold.ac.uk>
2024-11-25 11:40:59 +0000

    added font stuff

--- OLED_STARTER/OLED_STARTER.ino
+++ OLED_STARTER/OLED_STARTER.ino
@@ -2,28 +2,21 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#include "bitmaps.h"
 
-#define screenWidth 128
-#define screenHeight 64
-#define SCREEN_ADDRESS 0x3D  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(screenWidth, screenHeight, &Wire, -1);
-
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
 
 
 void setup() {
   Serial.begin(9600);
-  display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3D); //could be 0x3C
   display.clearDisplay();
-}
+}//end setup
 
 
 void loop() {
   display.clearDisplay();
   //drawing code here
-  int in = map(analogRead(A0), 0, 1023, 0, 128);
-  //display.drawFastVLine(in, 0,64, WHITE);
-  display.fillRect(0, 0, in, 64,WHITE);
-  display.display();
-  delay(100);
-}
+  display.fillRect(0, 0, in, 64, WHITE);
+  display.display();//drawing code sent to display
+  delay(10);
+}//end loop
